7a2a80db08bab9a8b547659ed73dc862e46436b6
rafaelmcandeias <rafaelmcandeias@LAPTOP-FRLSGR86.localdomain>
2021-10-05 20:29:23 +0100

    a shell ja esta feita. os nomes dos tests tem que seguir um padrão que esta escrito em comentario na shell. o a) ja esta feito, mas nao testei mt

--- MP1/run.sh
+++ MP1/run.sh
@@ -1,22 +1,49 @@
 #!/bin/bash
 
-mkdir -p compiled images
+# Cria pastas necessarias, caso ausentes
 
-for i in sources/*.txt tests/*.txt; do
-	echo "Compiling: $i"
-    fstcompile --isymbols=syms.txt --osymbols=syms.txt $i | fstarcsort > compiled/$(basename $i ".txt").fst
+mkdir -p compiled images compiled/tests compiled/sources compiled/composed
+
+
+# Compila todos os transducers contidos na pasta sources
+# Cria uma pasta para os testes compilados do transducer
+
+for transducer in sources/*.txt; do
+	echo "Compiling: $transducer; Creating file: compiled/tests/$(basename $transducer ".txt")"
+    fstcompile --isymbols=syms.txt --osymbols=syms.txt $transducer | fstarcsort > compiled/sources/$(basename $transducer ".txt").fst
+    mkdir -p compiled/tests/$(basename $transducer ".txt")
 done
 
+# Compila todos os transducers contidos na pasta testes
+# Todos os ficheiros tests tem que ser o nome do transducerXX, XX sao numeros
 
-# TODO
-
-echo "Testing the transducer 'converter' with the input 'tests/numeroR.txt' (generating pdf)"
-fstcompose compiled/numeroR.fst compiled/converter.fst | fstshortestpath > compiled/numeroA.fst
-
-for i in compiled/*.fst; do
-	echo "Creating image: images/$(basename $i '.fst').pdf"
-    fstdraw --portrait --isymbols=syms.txt --osymbols=syms.txt $i | dot -Tpdf > images/$(basename $i '.fst').pdf
+for transducer in tests/*.txt; do
+	echo "Compiling: $transducer and storing it in compiled/${transducer::-6}/$(basename $transducer ".txt").fst"
+    fstcompile --isymbols=syms.txt --osymbols=syms.txt $transducer | fstarcsort > compiled/${transducer::-6}/$(basename $transducer ".txt").fst
 done
 
-echo "Testing the transducer 'converter' with the input 'tests/numeroR.txt' (stdout)"
-fstcompose compiled/numeroR.fst compiled/converter.fst | fstshortestpath | fstproject --project_output=true | fstrmepsilon | fsttopsort | fstprint --acceptor --isymbols=./syms.txt
+# Cria um transducer composed para verificar os inputs
+
+for source in compiled/sources/*.fst; do
+    for teste in compiled/tests/$(basename $source ".fst")/*fst; do
+        echo "Testing the transducer $(basename $source) with the input $(basename $teste) (generating pdf)"
+        fstcompose $teste $source | fstshortestpath > compiled/composed/$(basename $teste ".fst")R.fst
+        fstcompose $teste $source | fstshortestpath | fstproject --project_output=true | fstrmepsilon | fsttopsort | fstprint --acceptor --isymbols=./syms.txt
+    done
+done
+
+# Cria a imagem em pdf dos transducers source e composed compilados
+
+for transducer in compiled/sources/*.fst compiled/composed/*.fst; do
+	echo "Creating image from transducer $transducer"
+    fstdraw --portrait --isymbols=syms.txt --osymbols=syms.txt $transducer | dot -Tpdf > images/$(basename $transducer '.fst').pdf
+done
+
+# Cria a imagem em pdf dos transducers test compilados
+
+for pastaDeTestes in compiled/tests/*; do
+    for teste in $pastaDeTestes/*.fst; do
+        echo "Creating image from transducer: $teste"
+        fstdraw --portrait --isymbols=syms.txt --osymbols=syms.txt $teste | dot -Tpdf > images/$(basename $teste '.fst').pdf
+    done
+done
